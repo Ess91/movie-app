25b06be6914c7ff24433984df3fa2ceec1a29da0
Esther <eagyare91@gmail.com>
2021-01-29 22:05:26 +0000

    debugging

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bb41a97136b818b55fe1a8976faeb604&page=1'
-
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=bb41a97136b818b55fe1a8976faeb604&query="'
-
-//console.log(SEARCH_API)
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
-//Get initial movies
+// Get initial movies
 getMovies(API_URL)
 
 async function getMovies(url) {
@@ -18,9 +14,7 @@ async function getMovies(url) {
     const data = await res.json()
 
     showMovies(data.results)
-
 }
-
 
 function showMovies(movies) {
     main.innerHTML = ''
@@ -28,33 +22,28 @@ function showMovies(movies) {
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
 
-        const movieElement = document.createElement('div')
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
 
-        movieElement.classList.add('movie')
-
-        movieElement.innerHTML = `
-    
+        movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
-                <h3>Overview</h3>
-               ${overview}
-            </div>
-       
-    `
-
-        main.appendChild(movieElement)
-
-    });
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)
+    })
 }
 
 function getClassByRate(vote) {
-    if (vote >= 8) {
+    if(vote >= 8) {
         return 'green'
-    } else if (vote >= 5) {
+    } else if(vote >= 5) {
         return 'orange'
     } else {
         return 'red'
@@ -66,7 +55,7 @@ form.addEventListener('submit', (e) => {
 
     const searchTerm = search.value
 
-    if (searchTerm && searchTerm !== '') {
+    if(searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm)
 
         search.value = ''
@@ -74,3 +63,4 @@ form.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
+
